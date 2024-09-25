9f8f9ac3a3df11bd875f13f2d819bb29e881083b
liljepim <95076212+liljepim@users.noreply.github.com>
2024-09-25 19:53:52 +0800

    Added code from Homework 1

--- CSOPESY/CSOPESY.cpp
+++ CSOPESY/CSOPESY.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <stdlib.h>
 
-int main()
+typedef std::string String;
+
+void printMenu() {
+	std::cout << "     ____. ________              \n";
+	std::cout << "    |    |/  _____/  ____  ______\n";
+	std::cout << "    |    /   \\  ___ /  _ \\/  ___/\n";
+	std::cout << "/\\__|    \\    \\_\\  (  <_> )___ \\ \n";
+	std::cout << "\\________|\\______  /\\____/____  >\n";
+	std::cout << "                 \\/           \\/ \n";
+	std::cout << "Hello, welcome to the GabOS command line!\n";
+	std::cout << "Type 'exit' to quit, 'clear' to clear the screen\n";
+
+}
+
+int
+main()
 {
-    std::cout << "Hello World!\n";
+	printMenu();
+	String command = "";
+	bool isRunning = true;
+	while (isRunning)
+	{
+		std::cout << "Enter a command: ";
+		std::cin >> command;
+
+		if (!command.compare("initialize"))
+			std::cout << command + " recognized. Doing something.\n";
+		else if (!command.compare("screen"))
+			std::cout << command + " recognized. Doing something.\n";
+		else if (!command.compare("scheduler-test"))
+			std::cout << command + " recognized. Doing something.\n";
+		else if (!command.compare("scheduler-stop"))
+			std::cout << command + " recognized. Doing something.\n";
+		else if (!command.compare("report-util"))
+			std::cout << command + " recognized. Doing something.\n";
+		else if (!command.compare("clear")) {
+			system("cls");
+			printMenu();
+		}
+
+		else if (!command.compare("exit"))
+		{
+			std::cout << command + " recognized. Exiting OS.\n";
+			isRunning = false;
+		}
+		else
+			std::cout << command + " not  recognized.\n";
+	}
+	return 0;
 }
