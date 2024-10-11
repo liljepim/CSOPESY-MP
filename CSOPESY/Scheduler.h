1c93b24c581b2c3a06e500477f53759d66c73d5b
gab-p-s <88660725+gab-p-s@users.noreply.github.com>
2024-10-11 20:12:44 +0800

    edited Scheduler files

--- CSOPESY/Scheduler.h
+++ CSOPESY/Scheduler.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <iostream>
 #include <map>
+#include "Typedef.h"
+#include "ConsoleManager.h"
+
+typedef std::string String;
 
 class Scheduler
 {
@@ -11,19 +15,13 @@ public:
 
 private:
 	static Scheduler* sharedInstance;
-	std::map<String, int> configVars = 
-	{
-		{"num-cpu", 0},
-		{"quantum-cycles", 0},
-		{"batch-process-freq", 0},
-		{"min-ins", 0},
-		{"mix-ins", 0},
-		{"delay-per-exec", 0}
-	};
-	String scheduler;
+	static std::map<String, int> configVars;
+	static String scheduler;
 	//next 4 lines pasted as is; code not tested yet
+	/*
 	ConsoleManager();
 	~ConsoleManager() = default;
 	ConsoleManager(ConsoleManager const&) {};
 	ConsoleManager& operator=(ConsoleManager const&) {};
-}
+	*/
+};
