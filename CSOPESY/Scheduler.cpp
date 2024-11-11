f62c78b6fc22c78ac106477d2c191b23bbb324aa
liljepim <95076212+liljepim@users.noreply.github.com>
2024-11-11 19:53:46 +0800

    Changes to Flat Memory Allocator

--- CSOPESY/Scheduler.cpp
+++ CSOPESY/Scheduler.cpp
@@ -17,22 +17,75 @@ extern std::mutex mtx;
 class IMemoryAllocator
 {
 public:
-	virtual void* allocate(int size) = 0;
+	virtual void* allocate(size_t size) = 0;
 	virtual void deallocate(void* ptr) = 0;
 };
 
 class FlatMemoryAllocator : public IMemoryAllocator
 {
 public:
-	FlatMemoryAllocator(int maximumSize)
+	FlatMemoryAllocator(size_t maximumSize)
 	{
 		this->maximumSize = maximumSize;
 		this->allocatedSize = 0;
+		this->memory.reserve(maximumSize);
+		initializeMemory();
+	}
+
+	~FlatMemoryAllocator()
+	{
+		memory.clear();
+	}
+
+	void* allocate(size_t size) override
+	{
+		for(size_t i = 0; i < maximumSize - size + 1; ++i)
+		{
+			if(!allocationMap[i] && canAllocateAt(i, size))
+			{
+				allocateAt(i, size);
+				return &memory[i];
+			}
+		}
+
+		return nullptr;
+	}
+
+	void deallocate(void* ptr) override
+	{
+		size_t index = static_cast<char*>(ptr) - &memory[0];
+		if(allocationMap[index])
+		{
+			deallocateAt(index);
+		}
 	}
 private:
-	int maximumSize;
-	int allocatedSize;
-	
+	size_t maximumSize;
+	size_t allocatedSize;
+	std::vector<char> memory;
+	std::unordered_map<size_t, bool> allocationMap;
+
+	void initializeMemory()
+	{
+		std::fill(memory.begin(), memory.end(), '.');
+		std::fill(allocationMap.begin(), allocationMap.end(), false);
+	}
+
+	bool canAllocateAt(size_t index, size_t size) const
+	{
+		return (index + size <= maximumSize);
+	}
+
+	void allocateAt(size_t index, size_t size)
+	{
+		std::fill(std::next(allocationMap.begin(), index), std::next(allocationMap.begin(), index + size), true);
+		allocatedSize += size;
+	}
+
+	void deallocateAt(size_t index)
+	{
+		allocationMap[index] = false;
+	}
 };
 
 Scheduler* Scheduler::sharedInstance = nullptr;
